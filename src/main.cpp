8f7ca1d8db8eba41fe1cf7c1af0601a5432d3cb0
gvanhoy <gvanhoy@email.arizona.edu>
2018-02-01 07:39:06 -0700

    Yes the timer actually works.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "led.h"
 #include "speaker.h"
 #include "switch.h"
+#include "timers.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -16,50 +17,58 @@ typedef enum stateType_num{
 
 int main(){
 
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
   initLED();
-  initSpeaker();
-  initSwitches();
-  unsigned int state = 0;
+  // initSpeaker();
+  // initSwitches();
+  initTimer0();
 
+  // PORTH |= (1 << PORTH5);
+  DDRB |= (1 << DDB7);
+  // PORTB |= (1 << PORTB7);
+
+  // unsigned int state x= 0;
   while(1){
-    switch(state){
-      case 0:
-        unarmedState();
-        if(!(PIND & (1 << PIND2))){
-          state = 1;
-          _delay_ms(1000);
-        }
-        break;
-      case 1:
-        unarmedState();
-        if(!(PIND & (1 << PIND1))){
-          state = 2;
-          _delay_ms(1000);
-        }
-        else if(!(PIND & (1 << PIND2))){
-          state = 0;
-          _delay_ms(1000);
-        }
-        break;
-      case 2:
-        standbyState();
-        _delay_ms(3000);
-        state = 3;
-        break;
-      case 3:
-        armedState();
-        if(!(PIND & (1 << PIND2))){
-          state = 4;
-        }
-        break;
-      case 4:
-        // alarmState();
-        chirp();
-        break;
-    }
-
+    delaySecond();
+    PORTB ^= (1 << PORTB7);
+    // Serial.println("Tick");
+    // PORTB ^= (1 << PORTB7);
+    // switch(state){
+    //   case 0:
+    //     unarmedState();
+    //     if(!(PIND & (1 << PIND2))){
+    //       state = 1;
+    //       _delay_ms(1000);
+    //     }
+    //     break;
+    //   case 1:
+    //     unarmedState();
+    //     if(!(PIND & (1 << PIND1))){
+    //       state = 2;
+    //       _delay_ms(1000);
+    //     }
+    //     else if(!(PIND & (1 << PIND2))){
+    //       state = 0;
+    //       _delay_ms(1000);
+    //     }
+    //     break;
+    //   case 2:
+    //     standbyState();
+    //     _delay_ms(3000);
+    //     state = 3;
+    //     break;
+    //   case 3:
+    //     armedState();
+    //     if(!(PIND & (1 << PIND2))){
+    //       state = 4;
+    //     }
+    //     break;
+    //   case 4:
+    //     // alarmState();
+    //     chirp();
+    //     break;
+    // }
   }
 
   return 0;
--- src/switch.cpp
+++ src/switch.cpp
@@ -9,7 +9,7 @@
 void initSwitches(){
   // input
   DDRD &= ~((1 << DDD3) | (1 << DDD2) | (1 << DDD1));
-  
+
   // enabling pull-up resistor
   PORTD |= (1 << PORTD3) | (1 << PORTD2) | (1 << PORTD1);
 }
