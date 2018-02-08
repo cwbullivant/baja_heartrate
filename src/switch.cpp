719bc703aa57f85e5c6cef45aa2da19376d5c4fb
gvanhoy <gvanhoy@email.arizona.edu>
2018-02-08 08:47:04 -0700

    Added comment

--- src/switch.cpp
+++ src/switch.cpp
@@ -10,6 +10,8 @@ void initSwitches(){
   // input
   DDRD &= ~((1 << DDD2) | (1 << DDD1) | (1 << DDD0));
 
+  // this is comment
+
   // enabling pull-up resistor
   PORTD |= (1 << PORTD2) | (1 << PORTD1) | (1 << PORTD0);
 
